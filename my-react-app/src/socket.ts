401afccf6c1595df6c21c7d0717bebb389ab0b52
Dharun Kumar <dharun.logic@gmail.com>
2026-03-09 23:30:54 +0530

    Comment out SERVER URL definition

--- my-react-app/src/socket.ts
+++ my-react-app/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SERVER = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000';
+// const SERVER = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000';
 export const socket: Socket = io('anonygame-production.up.railway.app');
 
 export default socket;
